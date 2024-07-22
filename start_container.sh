8fa386cc27278054131be10240db9ebc8af26f54
ANIlAWS0903 <169065479+ANIlAWS0903@users.noreply.github.com>
2024-07-22 12:17:23 +0530

    update for app deploy script

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull anilaws0903/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 anilaws0903/simple-python-flask-app
